7ba1214336ed650efa6429ec627e5767bb4ec505
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-02-15 11:28:56 -0600

    file renaming for use in other repos

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
 sfdx force:user:permset:assign -n VolunteeringApp
-sfdx force:data:tree:import -p data/masterImportPlan.json
-sfdx force:apex:execute -f scripts/setup.cls
+sfdx force:data:tree:import -p data/VolunteerImportPlan.json
+sfdx force:apex:execute -f scripts/VolunteerSetupScript.cls
 sfdx force:org:open
---
+++ package2.sh
@@ -0,0 +1,1 @@
+sfdx force:package2:version:create -d force-app -w 20
